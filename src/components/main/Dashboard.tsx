2197ad00713b5d3b328723048332a0d0ac6b5a70
shumman <shumman2007@hotmail.com>
2024-07-30 00:13:09 +0500

    added remaining

--- src/components/main/Dashboard.tsx
+++ src/components/main/Dashboard.tsx
@@ -5,9 +5,32 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import DoughnutChart from "../charts/charts";
 import SessionUsageChart from "../charts/UsageChart";
+import { Chart, ArcElement, RadialLinearScale, CategoryScale, LinearScale, PointElement, LineElement, BarElement } from 'chart.js'
 import axios from 'axios'
 import PieChart from "../charts/pieChart";
+
+
 // import Bowser from 'bowser';
+import { Bar, Line, PolarArea } from "react-chartjs-2"
+Chart.register(RadialLinearScale);
+Chart.register(CategoryScale);
+Chart.register(LinearScale);
+Chart.register(ArcElement);
+Chart.register(PointElement);
+Chart.register(LineElement);
+Chart.register(BarElement);
+const options: any = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "bottom",
+        },
+        title: {
+            display: true,
+            text: "Pie Chart Example",
+        },
+    },
+};
 type DomainsData = { domains: string }[]
 export default function DashboardContainer({ domainsData ,id}: { domainsData: DomainsData,id:number }) {
     const [activeDomain, setActiveDomain] = useState(domainsData[0])
@@ -59,7 +82,7 @@ export default function DashboardContainer({ domainsData ,id}: { domainsData: Do
                         <StatCard number={domainDashboardQuery.data!.total_users} label="Total Users" />
                         {/* <StatCard number="5" label="Total domain verified" /> */}
                     </div>
-                    <div className="flex justify-center items-start mt-8 gap-4">
+                    <div className="flex justify-center flex-wrap items-start mt-8 gap-4">
                         <div className=" rounded-lg  ">
                             <SessionUsageChart />
                             {/* <div className="text-center mt-4"></div> */}
@@ -70,6 +93,41 @@ export default function DashboardContainer({ domainsData ,id}: { domainsData: Do
                         {domainDashboardQuery.data.country_list.length != 0 && <div>
                             <PieChart labels={domainDashboardQuery.data.country_list} data={domainDashboardQuery.data.country_total_list} />
                         </div>}
+                        <div className="flex gap-4">
+                            <div>
+                                <Line className="!h-[20rem] !w-full" options={options} data={{
+                                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+                                    datasets: [{
+                                        label: 'No Of Users',
+                                        data: [65, 59, 80, 81, 56, 55, 40],
+                                        fill: false,
+                                        borderColor: '#FF3B3B',
+                                        tension: 0.1
+                                    }]
+                                }} />
+                            </div>
+                            <div>
+                                <PolarArea className="!h-[20rem] !w-full" options={options} data={{
+                                    labels: [
+                                        'Safari',
+                                        'Chrome',
+                                        'Microsoft Edge',
+                                        'FireFox',
+                                    ],
+                                    datasets: [{
+                                        label: 'My First Dataset',
+                                        data: [11, 16, 7, 3],
+                                        backgroundColor: [
+                                            'rgb(255, 99, 132)',
+                                            'rgb(75, 192, 192)',
+                                            'rgb(255, 205, 86)',
+                                            'rgb(201, 203, 207)',
+                                        ]
+                                    }]
+                                }} />
+                            </div>
+                        </div>
+                        
                     </div>
 
                 </>}
